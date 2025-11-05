04f193f859911e8e2ede1fb4786f827af9366a49
Dmitriy Onshin <donshin@ozon.ru>
2025-11-05 22:08:24 +0300

    add request latency metric

--- src/main/kotlin/ru/quipy/payments/logic/PaymentServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentServiceImpl.kt
@@ -2,6 +2,7 @@ package ru.quipy.payments.logic
 
 import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.MeterRegistry
+import io.prometheus.metrics.core.metrics.Summary
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -13,6 +14,7 @@ import java.util.*
 import java.util.concurrent.Executors
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
+import kotlin.time.toDuration
 
 
 @Service
@@ -30,12 +32,18 @@ class PaymentSystemImpl(
     private val failCounter =
         Counter.builder("http_request_fail_pay").description("Counts the number of fail pay").register(meterRegistry)
 
+    private val requestLatency =
+        Summary.builder().name("request_latency").quantile(0.85).build()
+
     private val maxRetries = 25
 
     override fun submitPaymentRequest(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long) {
         for (account in paymentAccounts) {
             for (i in 1..maxRetries) {
+                val start = System.currentTimeMillis()
                 val res = account.performPaymentAsync(paymentId, amount, paymentStartedAt, deadline)
+                val duration = System.currentTimeMillis() - start
+                requestLatency.observe(duration.toDouble())
                 if (res) {
                     successCounter.increment()
                     break
